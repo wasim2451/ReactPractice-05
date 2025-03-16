3085ab6bb5654a1991207ba046c19bb1bf947f03
wasim2451 <wasimakhtar786khan@gmail.com>
2025-03-17 04:35:34 +0530

    Commited ImageSlider

---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { sculptureList } from './data'
+export function ImageSlider() {
+    //state variables index,showMore
+    const [index, setIndex] = useState(0);
+    const [showMore, setshowMore] = useState(false);
+    const hasNext=index>=0&&index!=sculptureList.length-1; // true
+    const hasPrev=index<=sculptureList.length-1&&index!=0; // true
+    function handleClickNext() {
+        if(index===sculptureList.length-1){
+            setDisable(true);
+        }else{
+            setIndex(index+1);
+        }
+    }
+    function handleClickPrevious() {
+        index < 0 ? setIndex(0) :
+            setIndex(index - 1);
+    }
+    function descriptionHandler() {
+        setshowMore(!showMore);
+    }
+    return (
+        <>
+            <button onClick={handleClickNext} disabled={!hasNext}>Next</button>
+            <button onClick={handleClickPrevious} disabled={!hasPrev}>Previous</button>
+            <p>{index + 1} out of {sculptureList.length}</p>
+            <div>{sculptureList[index].name}</div>
+            <img src={sculptureList[index].url} alt="" style={{
+                'width': '30%'
+            }} />
+            <p>{sculptureList[index].artist}</p>
+            <p><button onClick={descriptionHandler}>Show Description</button></p>
+            <p>{
+                showMore ? sculptureList[index].description : null
+            }</p>
+        </>
+    )
+}
