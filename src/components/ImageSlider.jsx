4757608fd3019448917ed667559e5e22eab09407
wasim2451 <wasimakhtar786khan@gmail.com>
2025-03-17 16:11:42 +0530

    Committed mistake of unnecessary states disable and setDisable

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -4,18 +4,13 @@ export function ImageSlider() {
     //state variables index,showMore
     const [index, setIndex] = useState(0);
     const [showMore, setshowMore] = useState(false);
-    const hasNext=index>=0&&index!=sculptureList.length-1; // true
-    const hasPrev=index<=sculptureList.length-1&&index!=0; // true
+    const hasNext = index >= 0 && index != sculptureList.length - 1; // true // on index = length -->false
+    const hasPrev = index <= sculptureList.length - 1 && index != 0; // true // on index = 0 -->false 
     function handleClickNext() {
-        if(index===sculptureList.length-1){
-            setDisable(true);
-        }else{
-            setIndex(index+1);
-        }
+        setIndex(index + 1);
     }
     function handleClickPrevious() {
-        index < 0 ? setIndex(0) :
-            setIndex(index - 1);
+        setIndex(index - 1);
     }
     function descriptionHandler() {
         setshowMore(!showMore);
@@ -24,6 +19,7 @@ export function ImageSlider() {
         <>
             <button onClick={handleClickNext} disabled={!hasNext}>Next</button>
             <button onClick={handleClickPrevious} disabled={!hasPrev}>Previous</button>
+            {/* {On hasNext or hasPrev false disabled=true}  */}
             <p>{index + 1} out of {sculptureList.length}</p>
             <div>{sculptureList[index].name}</div>
             <img src={sculptureList[index].url} alt="" style={{
